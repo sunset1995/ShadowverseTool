ad623f917eb97c250e94df400712b32cec3093bd
Sunset <s2821d3721@gmail.com>
2018-07-14 16:38:40 +0800

    Add mischallous function

--- util.py
+++ util.py
@@ -1,5 +1,8 @@
+import os
+import cv2
 import mss
 import time
+import pyautogui
 import threading
 import numpy as np
 from PIL import Image
@@ -46,3 +49,42 @@ class ScreenShotter:
         Return current screen in 1st monitor as numpy.
         '''
         return np.array(self.pil_img)
+
+
+def read_template(*path_sep):
+    return np.array(Image.open(
+        os.path.join(*path_sep)
+    ))
+
+
+def check_existed(screenshotter, temp_img, thres=0.96):
+    sim = cv2.matchTemplate(
+        screenshotter.get(), temp_img,
+        cv2.TM_CCORR_NORMED
+    )
+    _, max_val, _, max_loc = cv2.minMaxLoc(sim)
+    return max_val > thres
+
+
+def wait(screenshotter, temp_img, mode='on', thres=0.96):
+    assert(mode in ['on', 'off'])
+    while True:
+        existed = check_existed(screenshotter, temp_img, thres)
+        if (mode == 'on' and existed) or \
+            (mode == 'off' and not existed):
+            return
+
+
+def find_and_click(screenshotter, temp, thres=0.96, rel_w=0.5, rel_h=0.5):
+    sim = cv2.matchTemplate(
+        screenshotter.get(), temp,
+        cv2.TM_CCORR_NORMED
+    )
+    _, max_val, _, max_loc = cv2.minMaxLoc(sim)
+    if max_val < thres:
+        return False
+
+    x = int(round(max_loc[0] + temp.shape[1] * rel_w))
+    y = int(round(max_loc[1] + temp.shape[0] * rel_h))
+    pyautogui.click(x=x, y=y, interval=0.1)
+    return True
